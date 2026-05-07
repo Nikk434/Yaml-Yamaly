31ebdd86a947c4cdca80867469e851ccedbc6bfd
Nikk434 <nskshirsagar434@gmail.com>
2026-05-07 17:36:50 +0530

    async call on last seen

--- backend/app/dependencies/sesh_dep.py
+++ backend/app/dependencies/sesh_dep.py
@@ -1,10 +1,20 @@
+import asyncio
 from fastapi import Depends, HTTPException, Header
 from sqlalchemy.orm import Session
 from datetime import datetime
-
+from sqlalchemy.orm import joinedload
 from app.db.sessions import get_db
 from app.schemas.sessions_sch import Session as SessionModel
 from app.models.sessions import SessionContext
+
+def _update_last_seen(session_id, db):
+    try:
+        db.query(SessionModel).filter(
+            SessionModel.id == session_id
+        ).update({"last_seen_at": datetime.utcnow()})
+        db.commit()
+    except:
+        pass
 
 def get_current_session(
     x_session_token: str = Header(..., alias="X-Session-Token"),
@@ -12,6 +22,7 @@ def get_current_session(
 ) -> SessionContext:
     session = (
         db.query(SessionModel)
+        .options(joinedload(SessionModel.room))
         .filter(SessionModel.session_token == x_session_token)
         .first()
     )
@@ -19,12 +30,15 @@ def get_current_session(
     if not session:
         raise HTTPException(status_code=401, detail="Invalid session token")
 
-    session.last_seen_at = datetime.utcnow()
-    db.commit()
-
+    # session.last_seen_at = datetime.utcnow()
+    # db.commit()
+    asyncio.create_task(
+        asyncio.to_thread(_update_last_seen, session.id, db)
+    )
     return SessionContext(
         session_id = session.id,
         room_id=session.room_id,
         role=session.role,
-        last_seen_at=session.last_seen_at
+        last_seen_at=session.last_seen_at,
+        room_code=session.room.room_code,
     )
