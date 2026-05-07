59e82f6703181f1e41cf0507fb8fd005a80553b8
Nikk434 <nskshirsagar434@gmail.com>
2026-05-07 17:37:18 +0530

    db configs

--- backend/app/db/sessions.py
+++ backend/app/db/sessions.py
@@ -10,7 +10,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    pool_pre_ping=True
+    pool_size=5,
+    max_overflow=10,
+    pool_pre_ping=True,
+    pool_recycle=300
 )
 
 SessionLocal = sessionmaker(
